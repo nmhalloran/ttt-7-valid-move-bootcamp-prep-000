9c10e26b896dd54c35d46d5f0d5c14a36d07eaa3
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-13 17:49:25 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,10 @@
 # code your #valid_move? method here
 def valid_move?(board, token)
-  print position_taken?(board, token)
+  if token.between?(0, board.lenght) && position_taken?(board, token) == false
+    true
+  else
+    false
+  end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
