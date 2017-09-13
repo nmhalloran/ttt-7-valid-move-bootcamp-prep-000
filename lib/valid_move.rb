eabcd853025d8b134d4ae6a3020c5e0b3e05e84a
Nicholas Halloran <nmhalloran@hotmail.com>
2017-09-13 17:46:59 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,15 @@
 # code your #valid_move? method here
-
+def valid_move?(board, token)
+  print position_taken?(board.token)
+end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def position_taken?(board, token)
+  if board[token] == " " || board[token] == "" || board[token] == nil
+    false
+  elsif board[token] == "X" || board[token] == "O"
+    true
+  end
+end
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
